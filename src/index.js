09648e250be9b56917b854de5d4bbeacb54072a5
Denis Kulik <kulikdenis1996x@gmail.com>
2023-01-23 20:45:19 +0700

    connect the leaflest library

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import './index.html';
 import './libs/normalize.css';
+
+// Import CSS from Leaflet and plugins
+import 'leaflet/dist/leaflet.css';
+import 'leaflet.markercluster/dist/MarkerCluster.css';
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
+
+// Import images directly that got missed via the CSS imports above
+import 'leaflet/dist/images/marker-icon-2x.png';
+import 'leaflet/dist/images/marker-shadow.png';
+
+// Import JS from Leaflet and plugins.
+import 'leaflet/dist/leaflet';
+import 'leaflet.markercluster/dist/leaflet.markercluster';
+
 import './css/style.css';
 import './js/app';
