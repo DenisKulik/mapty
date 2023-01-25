b391534f40e1ffac2d65f56daf7666c7d1f4ef21
Denis Kulik <kulikdenis1996x@gmail.com>
2023-01-25 19:16:24 +0700

    add creating a new workout

--- src/js/Running.js
+++ src/js/Running.js
@@ -1,6 +1,8 @@
 import Workout from './Workout';
 
 export default class Running extends Workout {
+  type = 'running';
+
   constructor(coords, distance, duration, cadence) {
     super(coords, distance, duration);
     this.cadence = cadence;
--- src/js/UserInterface.js
+++ src/js/UserInterface.js
@@ -1,3 +1,6 @@
+import Running from './Running';
+import Cycling from './Cycling';
+
 export default class UserInterface {
   form = document.querySelector('.form');
   containerWorkouts = document.querySelector('.workouts');
@@ -9,12 +12,16 @@ export default class UserInterface {
 
   #map;
   #mapEvent;
+  #workouts = [];
 
   constructor() {
     this.#getPosition();
 
     this.form.addEventListener('submit', this.#newWorkout.bind(this));
-    this.inputType.addEventListener('change', this.#toggleElevationField);
+    this.inputType.addEventListener(
+      'change',
+      this.#toggleElevationField.bind(this)
+    );
   }
 
   #getPosition() {
@@ -60,19 +67,65 @@ export default class UserInterface {
       .classList.toggle('form__row--hidden');
   }
 
+  // eslint-disable-next-line consistent-return
   #newWorkout(e) {
     e.preventDefault();
 
-    // clear input fields
+    const validInputs = (...inps) => inps.every((inp) => Number.isFinite(inp));
+    const allPositive = (...inps) => inps.every((inp) => inp > 0);
+
+    // get data from form
+    const type = this.inputType.value;
+    const distance = Number(this.inputDistance.value);
+    const duration = Number(this.inputDuration.value);
+    const { lat, lng } = this.#mapEvent.latlng;
+    let workout;
+
+    if (type === 'running') {
+      const cadence = Number(this.inputCadence.value);
+
+      if (
+        !validInputs(distance, duration, cadence) ||
+        !allPositive(distance, duration, cadence)
+      ) {
+        // eslint-disable-next-line no-alert
+        return alert('Inputs have to be positive numbers');
+      }
+
+      workout = new Running([lat, lng], distance, duration, cadence);
+    }
+
+    if (type === 'cycling') {
+      const elevation = Number(this.inputElevation.value);
+
+      if (
+        !validInputs(distance, duration, elevation) ||
+        !allPositive(distance, duration)
+      ) {
+        // eslint-disable-next-line no-alert
+        return alert('Inputs have to be positive numbers');
+      }
+
+      workout = new Cycling([lat, lng], distance, duration, elevation);
+    }
+
+    // add a new object to workout array
+    this.#workouts.push(workout);
+
+    // render workout on map as marker
+    this.renderWorkoutMarker(workout);
+
+    // render workout on list
+
+    // hide form and clear input fields
     this.inputDistance.value = '';
     this.inputDuration.value = '';
     this.inputCadence.value = '';
     this.inputElevation.value = '';
+  }
 
-    // display marker
-    const { lat, lng } = this.#mapEvent.latlng;
-
-    L.marker([lat, lng], { opacity: 0.6 })
+  renderWorkoutMarker(workout) {
+    L.marker(workout.coords, { opacity: 0.6 })
       .addTo(this.#map)
       .bindPopup(
         L.popup({
@@ -80,7 +133,7 @@ export default class UserInterface {
           minWidth: 100,
           autoClose: false,
           closeOnClick: false,
-          className: 'running-popup',
+          className: `${workout.type}-popup`,
         })
       )
       .setPopupContent('Workout')
